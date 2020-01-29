f34ae376af632422a2f585ddd9ac0a3b64430681
neilquizon <neil.quizon@gmail.com>
2020-01-28 20:33:04 -0800

    Add project files.

---
+++ Tickets/Controllers/EventController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tickets.Services;
+
+namespace Tickets.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly TicketsDbContext _context;
+        public EventController(TicketsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GetAll() is automatically recognized as
+        // http://localhost:<port #>/api/Event
+        [HttpGet]
+        public IEnumerable<Event> GetAll()
+        {
+            return _context.Event.ToList();
+        }
+    }
+}
